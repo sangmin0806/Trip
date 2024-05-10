9a74654a643ba8dfdf6072d78b6ecfa1adc5162c
손다인 <dain0826@naver.com>
2024-05-10 12:09:03 +0900

    스키마수정

--- SSAFY_11th_Project_Trip/src/main/resources/schema.sql
+++ SSAFY_11th_Project_Trip/src/main/resources/schema.sql
@@ -1,18 +1,19 @@
+use ssafyproject;
+
+
 CREATE TABLE `Attraction` (
-	`content_id`	int	NOT NULL	COMMENT '받아온 데이터 내 content_id 를 PK 로 사용',
-	`thumbnail_image_id`	int	NOT NULL	COMMENT 'Auto Increment',
-	`title`	varchar(100)	NOT NULL,
-	`tel`	varchar(50)	NULL,
-	`description`	varchar(10000)	NULL	COMMENT '장소 설명'
+    `content_id` int NOT NULL COMMENT '받아온 데이터 내 content_id 를 PK 로 사용',
+    `thumbnail_image_id` int NOT NULL COMMENT '이미지 테이블의 id를 참조하는 외래 키',
+    `title` varchar(100) NOT NULL,
+    `tel` varchar(50) NULL,
+    `description` varchar(10000) NULL COMMENT '장소 설명',
+    PRIMARY KEY (`content_id`)
 );
 
--- UPDATE Attraction a
--- JOIN attraction_description ad ON a.content_id = ad.content_id
--- SET a.description = ad.overview;
 
--- INSERT INTO Attraction (content_id, thumbnail_image_id, title, tel, description)
--- SELECT content_id, 0, title, tel, NULL
--- FROM attraction_info;
+
+
+
 
 CREATE TABLE `Address` (
     `id` int NOT NULL AUTO_INCREMENT COMMENT 'Auto Increment',
@@ -26,48 +27,17 @@ CREATE TABLE `Address` (
 );
 
 
--- INSERT INTO Address ( content_id, sido_code, gugun_code, address, latitude, longitude)
--- SELECT  content_id, sido_code, gugun_code, CONCAT(addr1, ' ', addr2), latitude, longitude
--- FROM attraction_info;
 
-CREATE TABLE `Content_Type` (
-	`content_type_id`	int	NOT NULL	COMMENT '0 : 구분없음',
-	`content_id`	int	NOT NULL,
-	`type_name`	varchar(30)	NOT NULL
-);
+
+
 CREATE TABLE `Content_Type` (
     `content_type_id` int NOT NULL COMMENT '0 : 구분없음',
     `content_id` int NOT NULL,
     `type_name` varchar(30) NOT NULL,
     PRIMARY KEY (`content_type_id`, `content_id`)
 );
--- INSERT INTO Content_Type (content_type_id, content_id, type_name)
--- SELECT 
---     content_type_id,
---     content_id,
---     CASE 
---         WHEN content_type_id = 12 THEN '관광지'
---         WHEN content_type_id = 14 THEN '문화시설'
---         WHEN content_type_id = 15 THEN '축제공연행사'
---         WHEN content_type_id = 25 THEN '여행코스'
---         WHEN content_type_id = 28 THEN '레포츠'
---         WHEN content_type_id = 32 THEN '숙박'
---         WHEN content_type_id = 38 THEN '쇼핑'
---         WHEN content_type_id = 39 THEN '음식점'
---         ELSE '구분없음'
---     END AS type_name
--- FROM attraction_info;
 
-CREATE TABLE `SiDo` (
-	`sido_code`	int	NOT NULL,
-	`sido_name`	varchar(30)	NOT NULL
-);
 
-CREATE TABLE `GuGun` (
-	`gugun_code`	int	NOT NULL,
-	`sido_code`	int	NULL,
-	`gugun_name`	varchar(30)	NOT NULL
-);
 
 CREATE TABLE `members` (
 	`id`	int	NOT NULL AUTO_INCREMENT	 COMMENT 'Auto Increment',
@@ -81,12 +51,9 @@ CREATE TABLE `members` (
     PRIMARY KEY (`id`),
 	UNIQUE KEY `unique_user_id` (`user_id`)
 );
-ALTER TABLE `members`
-ADD UNIQUE INDEX `unique_user_id` (`user_id`);
 
--- INSERT INTO `ssafyproject`.`members` (`user_id`, `user_name`, `user_password`, `email`, `join_date`, `agreeYN`)
--- VALUES ('ssafy', '김싸피', '1234', 'ssafy@naver.com', NOW(), 1), 
---        ('admin', '관리자', '1234', 'admin@naver.com', NOW(), 1);
+
+
 
 
 CREATE TABLE IF NOT EXISTS `ssafyproject`.`board` (
@@ -100,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `ssafyproject`.`board` (
   INDEX `board_to_members_user_id_fk` (`user_id` ASC),
   CONSTRAINT `board_to_members_user_id_fk`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ssafyweb`.`members` (`user_id`))
+    REFERENCES `ssafyproject`.`members` (`user_id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4
@@ -128,13 +95,12 @@ CREATE TABLE `Review` (
 );
 
 CREATE TABLE `Image` (
-	`id`	int	NOT NULL	AUTO_INCREMENT COMMENT 'Auto Increment',
-	`url`	varchar(5000)	NULL
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Auto Increment',
+  `url` varchar(5000) NULL,
+  PRIMARY KEY (`id`)
 );
 
--- INSERT INTO Image (url)
--- SELECT first_image
--- FROM attraction_info;
+
 
 CREATE TABLE `ReviewImage` (
 	`review_id`	int	NOT NULL,
@@ -153,29 +119,9 @@ CREATE TABLE `Like` (
 	`user_id`	int	NOT NULL
 );
 
-ALTER TABLE `Attraction` ADD CONSTRAINT `PK_ATTRACTION` PRIMARY KEY (
-	`content_id`
-);
 
-ALTER TABLE `Address` ADD CONSTRAINT `PK_ADDRESS` PRIMARY KEY (
-	`id`
-);
 
-ALTER TABLE `Content_Type` ADD CONSTRAINT `PK_CONTENT_TYPE` PRIMARY KEY (
-	`content_type_id`
-);
 
-ALTER TABLE `SiDo` ADD CONSTRAINT `PK_SIDO` PRIMARY KEY (
-	`sido_code`
-);
-
-ALTER TABLE `GuGun` ADD CONSTRAINT `PK_GUGUN` PRIMARY KEY (
-	`gugun_code`
-);
-
-ALTER TABLE `Members` ADD CONSTRAINT `PK_MEMBERS` PRIMARY KEY (
-	`id`
-);
 
 ALTER TABLE `List` ADD CONSTRAINT `PK_LIST` PRIMARY KEY (
 	`id`
@@ -185,20 +131,19 @@ ALTER TABLE `Review` ADD CONSTRAINT `PK_REVIEW` PRIMARY KEY (
 	`id`
 );
 
-ALTER TABLE `Image` ADD CONSTRAINT `PK_IMAGE` PRIMARY KEY (
-	`id`
-);
 
 ALTER TABLE `Comment` ADD CONSTRAINT `PK_COMMENT` PRIMARY KEY (
 	`id`
 );
 
 ALTER TABLE `Attraction` ADD CONSTRAINT `FK_Image_TO_Attraction_1` FOREIGN KEY (
-	`thumbnail_iamge_id`
+	`thumbnail_image_id`
 )
 REFERENCES `Image` (
 	`id`
 );
+ALTER TABLE `Attraction` ADD INDEX `content_id_index` (`content_id`);
+
 
 ALTER TABLE `Address` ADD CONSTRAINT `FK_Attraction_TO_Address_1` FOREIGN KEY (
 	`content_id`
@@ -206,6 +151,7 @@ ALTER TABLE `Address` ADD CONSTRAINT `FK_Attraction_TO_Address_1` FOREIGN KEY (
 REFERENCES `Attraction` (
 	`content_id`
 );
+
 
 ALTER TABLE `Address` ADD CONSTRAINT `FK_SiDo_TO_Address_1` FOREIGN KEY (
 	`sido_code`
@@ -228,12 +174,7 @@ REFERENCES `Attraction` (
 	`content_id`
 );
 
-ALTER TABLE `GuGun` ADD CONSTRAINT `FK_SiDo_TO_GuGun_1` FOREIGN KEY (
-	`sido_code`
-)
-REFERENCES `SiDo` (
-	`sido_code`
-);
+
 
 ALTER TABLE `Members` ADD CONSTRAINT `FK_Image_TO_M_1` FOREIGN KEY (
 	`image_id`
@@ -325,4 +266,52 @@ ALTER TABLE `Like` ADD CONSTRAINT `FK_Members_TO_Like_1` FOREIGN KEY (
 REFERENCES `Members` (
 	`id`
 );
+
+
+
+INSERT INTO Image (url)
+SELECT DISTINCT first_image
+FROM attraction_info
+WHERE first_image != ''
+AND first_image NOT IN (SELECT url FROM Image);
+
+INSERT INTO Attraction (content_id, thumbnail_image_id, title, tel, description)
+SELECT ai.content_id, i.id, ai.title, ai.tel, NULL
+FROM attraction_info ai
+JOIN Image i ON ai.first_image = i.url;
+
+UPDATE Attraction a
+JOIN attraction_description ad ON a.content_id = ad.content_id
+SET a.description = ad.overview;
+
+-- 외래 키 제약 조건 잠시 해제
+SET FOREIGN_KEY_CHECKS=0;
+-- 데이터 삽입
+INSERT INTO Address (content_id, sido_code, gugun_code, address, latitude, longitude)
+SELECT content_id, sido_code, gugun_code, CONCAT(addr1, ' ', addr2), latitude, longitude
+FROM attraction_info;
+
+INSERT INTO Content_Type (content_type_id, content_id, type_name)
+SELECT 
+    content_type_id,
+    content_id,
+    CASE 
+        WHEN content_type_id = 12 THEN '관광지'
+        WHEN content_type_id = 14 THEN '문화시설'
+        WHEN content_type_id = 15 THEN '축제공연행사'
+        WHEN content_type_id = 25 THEN '여행코스'
+        WHEN content_type_id = 28 THEN '레포츠'
+        WHEN content_type_id = 32 THEN '숙박'
+        WHEN content_type_id = 38 THEN '쇼핑'
+        WHEN content_type_id = 39 THEN '음식점'
+        ELSE '구분없음'
+    END AS type_name
+FROM attraction_info;
+
+INSERT INTO `ssafyproject`.`members` (`user_id`, `user_name`, `user_password`, `email`, `join_date`, `agreeYN`)
+VALUES ('ssafy', '김싸피', '1234', 'ssafy@naver.com', NOW(), 1), 
+       ('admin', '관리자', '1234', 'admin@naver.com', NOW(), 1);
+       
+-- 외래 키 제약 조건 다시 활성화
+SET FOREIGN_KEY_CHECKS=1;
 

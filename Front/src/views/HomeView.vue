6d66b99e6befdedc37421b463d4dbf7f8700ead2
최승호 <choho97@naver.com>
2024-05-21 11:49:35 +0900

    메인화면 슬라이드 2

--- Front/src/views/HomeView.vue
+++ Front/src/views/HomeView.vue
@@ -1,41 +1,27 @@
 <script setup></script>
 
 <template>
-    <div>
-        <CarouselComponent />
-        </div>
+  <div class="full-width">
+    <carouselPicture />
+  </div>
 </template>
 
-  <script>
-  import CarouselComponent from '@/components/decoration/CarouselComponent.vue';
-  
-  export default {
-    name: 'App',
-    components: {
-      CarouselComponent
-    }
-  };
-  </script>
-<style>
-.image_slider {
-  width: 80%; /* 슬라이더의 전체 너비를 80%로 설정 */
-  margin: 0 auto; /* 슬라이더를 가운데 정렬 */
-}
+<script>
+import carouselPicture from '@/components/decoration/carouselPicture.vue';
 
-.carousel__item {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
+export default {
+  name: 'HomeView',
+  components: {
+    carouselPicture
+  }
+};
+</script>
 
-.carousel__item img {
-  width: 100%; /* 이미지의 너비를 슬라이더 너비에 맞춤 */
-  max-width: 600px; /* 이미지의 최대 너비를 600px로 설정 */
-  height: auto;
-}
-
-.carousel__item figcaption {
-  text-align: center;
+<style scoped>
+.full-width {
+  width: 100%; /* 너비 100% 설정 */
+  display: flex; /* Flexbox 레이아웃 사용 */
+  justify-content: center; /* 중앙 정렬 */
+  align-items: center; /* 수직 중앙 정렬 (선택 사항) */
 }
 </style>

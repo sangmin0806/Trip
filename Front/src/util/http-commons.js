89c39491ec072a0c5bdc81fd3c20158949fcbaa4
최승호 <choho97@naver.com>
2024-05-22 17:47:12 +0900

    병합후 업로드2

--- Front/src/util/http-commons.js
+++ Front/src/util/http-commons.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-<<<<<<< HEAD
 
 const { VITE_VUE_API_URL } = import.meta.env;
 
-=======
->>>>>>> eae5f170fa800f9fece9628269b8568f1913840c
 // local vue api axios instance
 function localAxios() {
   const instance = axios.create({
